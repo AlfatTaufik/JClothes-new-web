58f138c58bdc97c01a5e9070589e2736ebc11531
Ryxena <rubbenme@proton.me>
2024-06-11 04:18:48 +0700

    crud

---
+++ src/routes/page.jsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return(
+      <div>
+        <Navbar/>
+      </div>
+  );
+};
+
+export default page;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import { nextui } from '@nextui-org/react';
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html", "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
   darkMode: "class",
   theme: {
     extend: {
@@ -21,5 +25,6 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()]
+
 };
